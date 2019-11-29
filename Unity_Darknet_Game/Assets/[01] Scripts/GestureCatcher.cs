e2e13c44671757e7be943a63663247efd8f539c6
heatheart <46485177+OverHeatHeart@users.noreply.github.com>
2019-11-29 14:20:18 +0900

    Update GestureCatcher.cs

--- Unity_Darknet_Game/Assets/[01] Scripts/GestureCatcher.cs
+++ Unity_Darknet_Game/Assets/[01] Scripts/GestureCatcher.cs
@@ -9,9 +9,13 @@ public class GestureCatcher : MonoBehaviour
     private WaitForSeconds ws = new WaitForSeconds(0.25f);
 
     //손동작 변화에 따른 델리게이트
+    //현재 이 이벤트를 구독하는 클래스:
+    //TextManager, 현재 손동작(무기)를 표시하기위해 구독 중
+    //WeaponShooter, 현재 손동작(무기)에 맞는 총알 발사를 위해 구독 중
     public event VoidIntNotier OnGestured;
     void Start()
     {
+        //HandTracking 클래스로부터 오는 손동작 변화 구독
         HandTracking.instance.OnGestureChange += SubscribeGesture;
         weaponCnt = new bool[4];
         for(int i = 0; i < weaponCnt.Length; i++)
@@ -26,6 +30,7 @@ public class GestureCatcher : MonoBehaviour
     void SubscribeGesture(int gesture)
     {
         if (coolDown == false) return;
+        //아무 동작도 없는 경우 : 추후 업데이트 예정, 현재 아무 기능 없음
         if (gesture == 3)
         {
             OnGestured?.Invoke(gesture);
@@ -37,6 +42,7 @@ public class GestureCatcher : MonoBehaviour
     IEnumerator ShotWeapon(int g)
     {
         coolDown = false;
+        //재사용 대기시간 적용되어 총알 발사
         OnGestured?.Invoke(g);
         yield return ws;
         coolDown = true;
